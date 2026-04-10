70c465c992ef850e2af6c16024e1808955a77bb6
lerouxadam7-design <lerouxadam7@gmail.com>
2026-04-10 15:11:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -833,7 +833,7 @@ st.markdown("""
     <div>• All pictures taken from same height/zoom with similar lighting</div>
     <div>• Take pictures of all 4 front corners</div>
     <div>• Use manual centering</div>
-</div>
+    <div>• Use solid black contrasting background for pictures</div>
 """, unsafe_allow_html=True)
 
 if user_email:
